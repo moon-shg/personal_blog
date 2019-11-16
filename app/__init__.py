7dc8f636194580e6bcd89ce606bc115f8360118b
Mr.Moon <393773661@qq.com>
2019-11-16 23:26:05 +0800

    add a template files

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,13 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
 
+bootstrap = Bootstrap()
 
 def create_app():
     # 初始化应用
     app = Flask(__name__)
+
+    bootstrap.init_app(app)
 
     # 注册蓝本
     from .main import main as main_blueprint
