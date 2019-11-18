cb44e9d9e39a67aa242c620a7369a0020d7a9712
Mr.Moon <393773661@qq.com>
2019-11-18 18:53:00 +0800

    add mysql & init db

---
+++ config.py
@@ -0,0 +1,45 @@
+import os
+
+dasedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'Potato Cookbook')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER', 'smtp.qq.com')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT', '587'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', 'on', '1']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    @staticmethod
+    def init_app(app):
+        pass
+
+
+# 定义不同环境中的配置
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+                              "mysql+pymysql://st:123456@localhost:3306/blog_dev?charset=utf8mb4_unicode_ci"
+
+
+class TestingConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+                              "mysql+pymysql://st:123456@localhost:3306/blog_test?charset=utf8_general_ci"
+
+
+class ProductionConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('SQLALCHEMY_DATABASE_URI') or \
+                              "mysql+pymysql://st:123456@localhost:3306/blog_production?charset=utf8_general_ci"
+
+
+config = {
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
+}
